b93c6839827095034482f49aecda7d23fdbb2f9b
Henry Hemerly <henrique.hemerly@appliedtechnology.se>
2020-03-28 13:02:38 +0100

    db connected mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb+srv://henry:parking@park-app-otn0c.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'parkingDB';
+
+async function run() {
+  const client = await MongoClient.connect(url, { useNewUrlParser: true });
+  const db = client.db(dbName);
+
+  db.on('close', () => { process.stdout.write('closed connection\n'); });
+  db.on('reconnect', () => { process.stdout.write('reconnected\n'); });
+
+  const streets = db.collection('test');
+
+  streets.insertOne({a: 'youpy'}, (err, res) => {
+    client.close();
+
+    if (err) return process.stdout.write(err.message);
+
+    return process.stdout.write(`inserted count ${res.insertedCount} documents\n`);
+  });
+}
+
+run();
 
 const API_URL = 'https://openparking.stockholm.se/LTF-Tolken/v1/servicedagar/weekday/måndag?outputFormat=json&apiKey=231ca8a9-dc1a-41b7-a06f-87f61d585f1a';
 
