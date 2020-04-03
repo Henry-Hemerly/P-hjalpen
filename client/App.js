bf98f1ffd6c5c197e36b063d48c76abf79a6524a
Eddy <kiribaty88@gmail.com>
2020-04-03 17:09:42 +0200

    reafactored push notifications

--- client/App.js
+++ client/App.js
@@ -8,11 +8,13 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Onboarding1, Onboarding2 ,Onboarding3, Onboarding4, Onboarding5 } from './components/Onboarding';
 import HomeScreen from './components/Home';
 var PushNotification = require("react-native-push-notification");
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+
 
 
 
 PushNotification.configure({
-  // (optional) Called when Token is generated (iOS and Android)
+  // (optional) Called when Token is generated 
   onRegister: function(token) {
     console.log("TOKEN:", token);
   },
@@ -20,32 +22,20 @@ PushNotification.configure({
   // (required) Called when a remote or local notification is opened or received
   onNotification: function(notification) {
     console.log("NOTIFICATION:", notification);
-
     // process the notification
-
-    // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/react-native-push-notification-ios)
+    // required on iOS only 
     notification.finish(PushNotificationIOS.FetchResult.NoData);
   },
 
-  // ANDROID ONLY: GCM or FCM Sender ID (product_number) (optional - not required for local notifications, but is need to receive remote push notifications)
-  //senderID: "YOUR GCM (OR FCM) SENDER ID",
-
-  // IOS ONLY (optional): default: all - Permissions to register.
+  // IOS ONLY 
   permissions: {
     alert: true,
     badge: true,
     sound: true
   },
-
   // Should the initial notification be popped automatically
-  // default: true
   popInitialNotification: true,
 
-  /**
-   * (optional) default: true
-   * - Specified if permissions (ios) and token (android and ios) will requested or not,
-   * - if not, you must call PushNotificationsHandler.requestPermissions() later
-   */
   requestPermissions: true
 });
 function sendNotification() {
